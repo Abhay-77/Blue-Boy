04a45713f7f27b55d8e97a3f8854a7f86067efba
Abhay Murali <abhaymuralim9@gmail.com>
2024-10-02 16:13:40 +0530

    Fonts marmonica and purisa added

--- main/UI.java
+++ main/UI.java
@@ -4,13 +4,14 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.io.InputStream;
 
 
 public class UI {
 
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40,arial_80B;
+    Font marmonica,purisa;
     public boolean messageOn = false;
     int messageCounter=0;
     public String message = "";
@@ -19,8 +20,16 @@ public class UI {
 
     public UI(GamePanel gp) {
         this.gp = gp;
-        arial_40 = new Font("Arial",Font.PLAIN,40);
-        arial_80B = new Font("Arial",Font.BOLD,80);
+        try {
+            InputStream is = getClass().getResourceAsStream("/res/Font/Mister pixel/MP16OSF.ttf");
+            marmonica = Font.createFont(Font.TRUETYPE_FONT, is);
+            is = getClass().getResourceAsStream("/res/Font/Purisa Bold/Purisa Bold.ttf");
+            purisa = Font.createFont(Font.TRUETYPE_FONT, is);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 
     public void showMessage(String text) {
@@ -30,7 +39,7 @@ public class UI {
 
     public void draw(Graphics2D g2) {
         this.g2 = g2;
-        g2.setFont(arial_40);
+        g2.setFont(marmonica);
         g2.setColor(Color.white);
 
         if(gp.gameState == gp.playState) {
